cca81cbf0540adb5aa92eeb5bbd066c40bef3613
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-01-22 19:28:49 +0200

    new problem

--- LeetCode.Solutions/Top75/Topic02_TwoPointers/Is Subsequence.cs
+++ LeetCode.Solutions/Top75/Topic02_TwoPointers/Is Subsequence.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LeetCode.Solutions.Top75.Topic02_TwoPointers
+﻿namespace LeetCode.Solutions.Top75.Topic02_TwoPointers
 {
     internal class Is_Subsequence
     {
+        public bool IsSubsequence(string s, string t)
+        {
+            int i = 0;
+            int j = 0;
+            while (i < s.Length && j < t.Length)
+            {
+                if (s[i] == t[j])
+                {
+                    i++;
+                }
+                j++;
+            }
+
+            return i == s.Length;
+        }
     }
 }
