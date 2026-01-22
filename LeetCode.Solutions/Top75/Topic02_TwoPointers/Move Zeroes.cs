2ad6eb6ce7ce67dc7b4b2ff6cb3da335295712c7
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-01-22 19:25:15 +0200

    Two Pointers

---
+++ LeetCode.Solutions/Top75/Topic02_TwoPointers/Move Zeroes.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Solutions.Top75.Topic02_TwoPointers
+{
+    internal class Move_Zeroes
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int nonzero = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    int temp = nums[i];
+                    nums[i] = nums[nonzero];
+                    nums[nonzero] = temp;
+                    nonzero++;
+
+                }
+            }
+
+        }
+    }
+}
