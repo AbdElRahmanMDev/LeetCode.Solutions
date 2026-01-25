9a3ae848b458db5bb30237488eb004708705a002
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-01-25 15:39:12 +0200

    new problems

--- LeetCode.Solutions/Top75/Topic04_Stack/Removing Stars From a String.cs
+++ LeetCode.Solutions/Top75/Topic04_Stack/Removing Stars From a String.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LeetCode.Solutions.Top75.Topic04_Stack
+﻿namespace LeetCode.Solutions.Top75.Topic04_Stack
 {
     internal class Removing_Stars_From_a_String
     {
+        public string RemoveStars(string s)
+        {
+            var stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (c != '*')
+                {
+                    stack.Push(c);
+                }
+                else if (stack.Count > 0)
+                {
+                    stack.Pop();
+                }
+            }
+            return new string(stack.Reverse().ToArray());
+        }
     }
 }
