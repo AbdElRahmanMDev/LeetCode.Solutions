9fabc1d246c29e414c9accbb639f88ba8e7d0bf6
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-01-25 15:38:26 +0200

    stack2

--- LeetCode.Solutions/Top150/Topic04_Stack/Simplify Path.cs
+++ LeetCode.Solutions/Top150/Topic04_Stack/Simplify Path.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace LeetCode.Solutions.Top150.Topic04_Stack
 {
     internal class Simplify_Path
     {
+        public string SimplifyPath(string path)
+        {
+            var stack = new Stack<string>();
+            var parts = path.Split('/');
+
+            foreach (var part in parts)
+            {
+                if (part == "..")
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+                else if (part != "" && part != ".")
+                {
+                    stack.Push(part);
+                }
+            }
+            var result = new StringBuilder();
+            while (stack.Count > 0)
+            {
+                result.Insert(0, stack.Pop());
+                result.Insert(0, "/");
+            }
+            return result.Length == 0 ? "/" : result.ToString();
+        }
     }
+
 }
