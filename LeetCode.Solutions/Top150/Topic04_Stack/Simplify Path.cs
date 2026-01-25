c754b950b580cece973d8b31e419728a7317660f
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-01-25 15:36:20 +0200

    Stack

---
+++ LeetCode.Solutions/Top150/Topic04_Stack/Simplify Path.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Solutions.Top150.Topic04_Stack
+{
+    internal class Simplify_Path
+    {
+    }
+}
---
+++ LeetCode.Solutions/Top150/Topic04_Stack/Valid Parentheses.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Solutions.Top150.Topic04_Stack
+{
+    internal class Valid_Parentheses
+    {
+        public bool IsValid(string s)
+        {
+            Dictionary<char, char> dict =
+            new Dictionary<char, char>{
+                {')','('},
+                {']','['},
+                {'}','{'}
+            };
+            //"]["
+            Stack<char> stack = new Stack<char>();
+            foreach (var a in s)
+            {
+                if (dict.ContainsKey(a))
+                {
+                    var res = dict[a];
+                    if (stack.Count != 0 && res == stack.Peek())
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    stack.Push(a);
+                }
+            }
+
+            return stack.Count == 0;
+
+        }
+    }
+}
