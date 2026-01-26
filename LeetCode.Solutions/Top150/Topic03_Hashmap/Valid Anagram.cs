721bb8adb7407715ce9b83803ae91430eacdbbcd
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-01-26 16:46:28 +0200

    Daily Temp

--- LeetCode.Solutions/Top150/Topic03_Hashmap/Valid Anagram.cs
+++ LeetCode.Solutions/Top150/Topic03_Hashmap/Valid Anagram.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LeetCode.Solutions.Top150.Topic03_Hashmap
+﻿namespace LeetCode.Solutions.Top150.Topic03_Hashmap
 {
     internal class Valid_Anagram
     {
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+            int[] arr = new int[26];
+            for (int i = 0; i < s.Length; i++)
+            {
+                arr[s[i] - 'a']++;
+                arr[t[i] - 'a']--;
+            }
+            for (int i = 0; i < 26; i++)
+            {
+                if (arr[i] != 0)
+                    return false;
+            }
+            return true;
+        }
     }
 }
---
+++ LeetCode.Solutions/Top75/Topic04_Stack/Daily Temperatures.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Solutions.Top75.Topic04_Stack
+{
+    internal class Daily_Temperatures
+    {
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            int size = temperatures.Length;
+            int[] res = new int[size];
+            var stack = new Stack<int[]>();
+
+            for (int i = 0; i < size; i++)
+            {
+                while (stack.Count != 0 && stack.Peek()[0] < temperatures[i])
+                {
+                    var a = stack.Pop();
+                    res[a[1]] = i - a[1];
+                }
+                stack.Push(new int[] { temperatures[i], i });
+            }
+
+            return res;
+        }
+    }
+}
