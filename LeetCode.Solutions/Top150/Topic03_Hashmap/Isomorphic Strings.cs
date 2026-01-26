7e9f5e38d6ff3c430a29b8cd55b001dc38a0e9eb
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-01-26 22:00:00 +0200

    Isomorphic String

---
+++ LeetCode.Solutions/Top150/Topic03_Hashmap/Isomorphic Strings.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.Solutions.Top150.Topic03_Hashmap
+{
+    internal class Isomorphic_Strings
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+
+            if (s.Length != t.Length)
+                return false;
+
+            var mapST = new Dictionary<char, char>(); // s -> t
+            var mapTS = new Dictionary<char, char>(); // t -> s
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char a = s[i];
+                char b = t[i];
+
+                if (mapST.TryGetValue(a, out char mappedDb))
+                {
+                    if (mappedDb != b) return false;
+                }
+                else
+                {
+                    mapST[a] = b;
+                }
+                if (mapTS.TryGetValue(b, out char mappedA))
+                {
+                    if (mappedA != a) return false;
+                }
+                else
+                {
+                    mapTS[b] = a;
+                }
+            }
+            return true;
+
+        }
+    }
+}
