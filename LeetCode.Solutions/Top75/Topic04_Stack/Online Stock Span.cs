050c86b3a06c62033ddde6ef8bdb04c0583fac84
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-01-26 17:59:31 +0200

    Monotinic Stack

---
+++ LeetCode.Solutions/Top75/Topic04_Stack/Online Stock Span.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Solutions.Top75.Topic04_Stack
+{
+    internal class Online_Stock_Span
+    {
+        Stack<int[]> stack;
+        public Online_Stock_Span()
+        {
+            stack = new Stack<int[]>();
+
+        }
+
+        public int Next(int price)
+        {
+            int currentspan = 1;
+            while (stack.Count > 0 && stack.Peek()[0] <= price)
+            {
+                var prev = stack.Pop();
+                currentspan += prev[1];
+            }
+            stack.Push(new int[] { price, currentspan });
+            return currentspan;
+        }
+    }
+}
