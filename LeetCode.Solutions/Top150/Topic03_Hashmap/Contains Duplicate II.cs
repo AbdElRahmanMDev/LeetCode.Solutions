0c158c0789eb143d61fe39b3068ad7c2c84954a4
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-01-27 18:43:25 +0200

    Contains Duplicate

---
+++ LeetCode.Solutions/Top150/Topic03_Hashmap/Contains Duplicate II.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Solutions.Top150.Topic03_Hashmap
+{
+    internal class Contains_Duplicate_II
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+
+            var set = new HashSet<int>();
+            int l = 0;
+
+            for (int r = 0; r < nums.Length; r++)
+            {
+                if (r - l > k)
+                {
+                    set.Remove(nums[l]);
+                    l++;
+                }
+
+                if (set.Contains(nums[r]))
+                    return true;
+
+                set.Add(nums[r]);
+
+            }
+            return false;
+
+        }
+    }
+}
