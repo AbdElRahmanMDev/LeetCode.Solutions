c33c6704aa63914171d03938824a0e3050bd1e68
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-01-27 16:38:30 +0200

    Kth Largest Element

---
+++ LeetCode.Solutions/Shared/Topic05_HeapPriority Queue/Kth Largest Element in an Array.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Solutions.Shared.Topic05_HeapPriority_Queue
+{
+    internal class Kth_Largest_Element_in_an_Array
+    {
+        public int FindKthLargest(int[] nums, int k)
+        {
+            var pq = new PriorityQueue<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                pq.Enqueue(nums[i], nums[i]);
+
+                if (pq.Count > k)
+                {
+                    pq.Dequeue();
+                }
+            }
+
+            return pq.Peek();
+        }
+    }
+}
