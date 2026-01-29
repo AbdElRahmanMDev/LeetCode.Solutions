cb8727b40b46b25a1040d16b7ac51cb390e80ca7
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-01-29 16:33:19 +0200

    Two Problems Sliding Window

---
+++ LeetCode.Solutions/Top150/Topic05_Sliding Window/Longest Substring Without Repeating Characters.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace LeetCode.Solutions.Top150.Topic05_Sliding_Window;
+
+internal class Longest_Substring_Without_Repeating_Characters
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        int longest = 0;
+        int l = 0;
+        HashSet<int> set = new HashSet<int>();
+        for (int r = 0; r < s.Length; r++)
+        {
+            while (set.Contains(s[r]))
+            {
+                set.Remove(s[l]);
+                l++;
+            }
+            set.Add(s[r]);
+            longest = Math.Max(longest, r - l + 1);
+        }
+
+        return longest;
+    }
+}
---
+++ LeetCode.Solutions/Top150/Topic05_Sliding Window/Minimum Size Subarray Sum.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Solutions.Top150.Topic05_Sliding_Window;
+
+internal class Minimum_Size_Subarray_Sum
+{
+    public int MinSubArrayLen(int target, int[] nums)
+    {
+
+        int count = int.MaxValue;
+        int l = 0;
+        int sum = 0;
+        for (int r = 0; r < nums.Length; r++)
+        {
+            sum += nums[r];
+
+            while (sum >= target)
+            {
+                count = Math.Min(count, (r - l) + 1);
+                sum -= nums[l++];
+            }
+
+
+
+
+        }
+        return count == int.MaxValue ? 0 : count;
+
+    }
+}
