6717dc31eb87eefefe341a58f4d245e18bde4b34
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-01-29 17:18:10 +0200

    New HashSet Problem

---
+++ LeetCode.Solutions/Top150/Topic03_Hashmap/Longest Consecutive Sequence.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Solutions.Top150.Topic03_Hashmap
+{
+    internal class Longest_Consecutive_Sequence
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+            HashSet<int> set = new HashSet<int>(nums);
+            int longest = 0;
+            foreach (var s in set)
+            {
+                if (!set.Contains(s - 1))
+                {
+                    int length = 1;
+                    while (set.Contains(s + length))
+                    {
+                        length++;
+                    }
+
+                    longest = Math.Max(longest, length);
+                }
+            }
+
+            return longest;
+        }
+    }
+}
