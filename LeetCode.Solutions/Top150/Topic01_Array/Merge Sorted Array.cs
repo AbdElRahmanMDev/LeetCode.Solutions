7f0a29e5eb5cc1f4b9878f3d19fa9f337cfa2eb3
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-01-30 17:08:03 +0200

    Merge Sorted Array problem

--- LeetCode.Solutions/Top150/Topic01_Array/Merge Sorted Array.cs
+++ LeetCode.Solutions/Top150/Topic01_Array/Merge Sorted Array.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LeetCode.Solutions.Top150.Topic01_Array
+﻿namespace LeetCode.Solutions.Top150.Topic01_Array
 {
     internal class Merge_Sorted_Array
     {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int lastIndex = m + n - 1;
+
+            while (m >= 0 && n >= 0)
+            {
+                if (nums1[m - 1] > nums2[n - 1])
+                {
+                    nums1[lastIndex] = nums1[m - 1];
+                    m--;
+                }
+                else
+                {
+                    nums1[lastIndex] = nums2[n - 1];
+                    n--;
+                }
+                lastIndex--;
+            }
+
+            while (n >= 0)
+            {
+                nums1[lastIndex] = nums2[n - 1];
+                n--;
+                lastIndex--;
+            }
+
+        }
     }
 }
