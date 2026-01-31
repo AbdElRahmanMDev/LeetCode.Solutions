f3bd4534d2cfa48d69a3babc42fa89545f786d9d
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-01-31 18:30:07 +0200

    Valid Sudoku problem Matrix Folder

---
+++ LeetCode.Solutions/Top150/Topic010_Matrix/Valid Sudoku.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.Solutions.Top150.Topic010_Matrix
+{
+    internal class Valid_Sudoku
+    {
+        public bool IsValidSudoku(char[][] board)
+        {
+            Dictionary<int, HashSet<char>> rows = new Dictionary<int, HashSet<char>>();
+            Dictionary<int, HashSet<char>> cols = new Dictionary<int, HashSet<char>>();
+            Dictionary<string, HashSet<char>> sqrs = new Dictionary<string, HashSet<char>>();
+
+            for (int r = 0; r < 9; r++)
+            {
+                for (int c = 0; c < 9; c++)
+                {
+                    char value = board[r][c];
+                    string sqrkey = $"{r / 3},{c / 3}";
+                    if (value == '.')
+                        continue;
+                    if (!rows.ContainsKey(r))
+                    {
+                        rows[r] = new HashSet<char>();
+                    }
+                    if (!cols.ContainsKey(c))
+                    {
+                        cols[c] = new HashSet<char>();
+                    }
+                    if (!sqrs.ContainsKey(sqrkey))
+                    {
+                        sqrs[sqrkey] = new HashSet<char>();
+                    }
+
+                    if (!rows[r].Add(value) || !cols[c].Add(value) || !sqrs[sqrkey].Add(value))
+                        return false;
+                }
+            }
+            return true;
+
+        }
+    }
+}
