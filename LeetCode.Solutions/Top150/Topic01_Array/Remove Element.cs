a8a4090baa67afc486f29a75d85af3cbbe2f8289
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-01-31 20:16:38 +0200

    Remove Element

---
+++ LeetCode.Solutions/Top150/Topic01_Array/Remove Element.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Solutions.Top150.Topic01_Array
+{
+    internal class Remove_Element
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int result = 0;
+            foreach (var num in nums)
+            {
+                if (num != val)
+                {
+                    nums[result] = num;
+                    result++;
+                }
+            }
+            return result;
+        }
+
+    }
+}
