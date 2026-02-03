e05a2964fbfb4f51d0eeb42d77d4014cc7c73ed8
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-02-03 16:56:23 +0200

    Remove Duplicate From Sorted Array II

---
+++ LeetCode.Solutions/Top150/Topic01_Array/Remove Duplicates from Sorted Array II.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Solutions.Top150.Topic01_Array
+{
+    internal class Remove_Duplicates_from_Sorted_Array_II
+    {
+
+        public int RemoveDuplicates(int[] nums)
+        {
+            int result = 0;
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(nums[i]))
+                {
+                    dict[nums[i]]++;
+                    if (dict[nums[i]] <= 2)
+                    {
+                        nums[result] = nums[i];
+                        result++;
+                    }
+                }
+                else
+                {
+                    nums[result] = nums[i];
+                    result++;
+                    dict[nums[i]] = 1;
+                }
+            }
+            return result;
+        }
+
+    }
+}
