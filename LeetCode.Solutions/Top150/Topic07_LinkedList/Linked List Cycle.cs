a0a87deb5cc0c1f68baa41f40b87cd1c62f93ae6
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-02-04 16:53:48 +0200

    Cycle Linked List

---
+++ LeetCode.Solutions/Top150/Topic07_LinkedList/Linked List Cycle.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Solutions.Top150.Topic07_LinkedList
+{
+    internal class Linked_List_Cycle
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+
+        public bool HasCycle(ListNode head)
+        {
+            ListNode fast = head;
+            ListNode slow = head;
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+                if (slow.Equals(fast)) return true;
+            }
+            return false;
+        }
+    }
+}
