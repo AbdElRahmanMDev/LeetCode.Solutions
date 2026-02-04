c01fc5f3a91de030ce190d846df9aa458bb51300
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-02-04 18:18:01 +0200

    Odd Even Linked List.cs

---
+++ LeetCode.Solutions/Top75/Topic07_LinkedList/Odd Even Linked List.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Solutions.Top75.Topic07_LinkedList
+{
+    internal class Odd_Even_Linked_List
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+        public ListNode OddEvenList(ListNode head)
+        {
+            if (head == null || head.next == null)
+                return head;
+            ListNode currentEven = head;
+            ListNode currentOdd = head.next;
+            ListNode currentOddD = head.next;
+            while (currentOdd != null && currentOdd.next != null)
+            {
+                currentEven.next = currentOdd.next;
+                currentEven = currentEven.next;
+                currentOdd.next = currentEven.next;
+                currentOdd = currentOdd.next;
+            }
+
+            currentEven.next = currentOddD;
+
+            return head;
+
+        }
+    }
+}
