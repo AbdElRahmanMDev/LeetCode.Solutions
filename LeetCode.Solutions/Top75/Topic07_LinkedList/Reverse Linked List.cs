784ea6ea10021a6fd1ace3dcd555cfa7a4ba9658
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-02-04 14:24:41 +0200

    Reverse Linked List

---
+++ LeetCode.Solutions/Top75/Topic07_LinkedList/Reverse Linked List.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Solutions.Top75.Topic07_LinkedList
+{
+    public class Reverse_Linked_List
+    {
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+            public ListNode ReverseList(ListNode head)
+            {
+                ListNode prev = null;
+                ListNode current = head;
+
+                while (current != null)
+                {
+                    ListNode temp = current.next;
+                    current.next = prev;
+                    prev = current;
+                    current = temp;
+                }
+
+                return prev;
+            }
+        }
+    }
+
+}
