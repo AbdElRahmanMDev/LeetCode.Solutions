d93df9eda5aab83fa8b6fbf8cbe1c9f86b91b96d
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-02-05 15:34:38 +0200

    Delete the Middle Node of a Linked List

---
+++ LeetCode.Solutions/Top75/Topic07_LinkedList/Delete the Middle Node of a Linked List.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Solutions.Top75.Topic07_LinkedList
+{
+    internal class Delete_the_Middle_Node_of_a_Linked_List
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+
+        public ListNode DeleteMiddle(ListNode head)
+        {
+            if (head.next == null)
+                return null;
+            ListNode slow = head;
+            ListNode fast = head;
+            ListNode prev = null;
+
+            while (fast != null && fast.next != null)
+            {
+                prev = slow;
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            prev.next = prev.next.next;
+            return head;
+        }
+    }
+}
