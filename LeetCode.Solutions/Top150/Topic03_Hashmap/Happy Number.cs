cece6caa8f7e4e1cf87f97df4462cca9a3416a33
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-02-08 17:13:03 +0200

    Solve two Problems of today

---
+++ LeetCode.Solutions/Top150/Topic03_Hashmap/Happy Number.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Solutions.Top150.Topic03_Hashmap
+{
+    internal class Happy_Number
+    {
+        public bool IsHappy(int n)
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            while (n != 1)
+            {
+                if (set.Contains(n))
+                    return false;
+                set.Add(n);
+                n = Fun(n);
+            }
+            return true;
+        }
+
+        public int Fun(int n)
+        {
+            var current = 0;
+            while (n != 0)
+            {
+                var digit = n % 10;
+                current += digit * digit;
+                n = n / 10;
+            }
+            return current;
+        }
+    }
+}
---
+++ LeetCode.Solutions/Top150/Topic07_LinkedList/Partition List.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode.Solutions.Top150.Topic07_LinkedList
+{
+    internal class Partition_List
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public class Solution
+        {
+            public ListNode Partition(ListNode head, int x)
+            {
+
+                ListNode dummy = new ListNode();
+                ListNode dummy2 = new ListNode();
+                ListNode smaller = dummy;
+                ListNode greater = dummy2;
+                ListNode current = head;
+                while (current != null)
+                {
+                    if (current.val < x)
+                    {
+                        smaller.next = current;
+                        smaller = smaller.next;
+                    }
+                    else
+                    {
+                        greater.next = current;
+                        greater = greater.next;
+                    }
+                    current = current.next;
+                }
+                greater.next = null;
+                smaller.next = dummy2.next;
+                return dummy.next;
+            }
+        }
+    }
+}
