dc06bd32e175afe7b5a3f5d56cd3d12656f9e85e
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-02-10 19:53:50 +0200

    Length of Last Word

---
+++ LeetCode.Solutions/Top150/Topic01_Array/Length of Last Word.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Solutions.Top150.Topic01_Array
+{
+    internal class Length_of_Last_Word
+    {
+        public int LengthOfLastWord(string s)
+        {
+            s = s.Trim();
+            int r = s.Length - 1;
+            int count = 0;
+
+            while (r >= 0 && s[r] != ' ')
+            {
+                count++;
+                r--;
+            }
+
+            return count;
+        }
+    }
+}
