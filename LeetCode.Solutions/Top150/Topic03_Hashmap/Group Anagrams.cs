c8e36ab6c366ed07fe105778076d9651d81effad
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-02-10 19:07:16 +0200

    Group Anagrams

---
+++ LeetCode.Solutions/Top150/Topic03_Hashmap/Group Anagrams.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Solutions.Top150.Topic03_Hashmap
+{
+    internal class Group_Anagrams
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            Dictionary<string, IList<string>> map = new Dictionary<string, IList<string>>();
+            foreach (var s in strs)
+            {
+                var character = s.ToCharArray();
+                Array.Sort(character);
+                var sortedstring = new String(character);
+                if (!map.ContainsKey(sortedstring))
+                {
+                    map[sortedstring] = new List<string>();
+
+                }
+                map[sortedstring].Add(s);
+            }
+
+            var x = map.Select(x => x.Value).ToList();
+            return x;
+        }
+    }
+}
