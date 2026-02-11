2113c15e0f8361a83ca3b71d90a053821d0f5229
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-02-11 18:56:02 +0200

    Symmetric Tree

---
+++ LeetCode.Solutions/Top150/Topic08_BinarySearchTree/Symmetric Tree.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.Solutions.Top150.Topic08_BinarySearchTree
+{
+    internal class Symmetric_Tree
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null) return true;
+            return IsMirror(root.left, root.right);
+        }
+
+        private bool IsMirror(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null) return true;
+            if (p == null || q == null) return false;
+            if (p.val != q.val) return false;
+
+            return IsMirror(p.left, q.right) &&
+                   IsMirror(p.right, q.left);
+        }
+    }
+}
