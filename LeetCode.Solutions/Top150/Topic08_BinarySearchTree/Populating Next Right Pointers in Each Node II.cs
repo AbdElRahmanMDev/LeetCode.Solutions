b0998ae7f585b17bd17eb250b434f06ca9b4037f
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-02-16 20:25:01 +0200

    Populating Next Right Pointer

---
+++ LeetCode.Solutions/Top150/Topic08_BinarySearchTree/Populating Next Right Pointers in Each Node II.cs
@@ -0,0 +1,54 @@
+﻿namespace LeetCode.Solutions.Top150.Topic08_BinarySearchTree
+{
+    internal class Populating_Next_Right_Pointers_in_Each_Node_II
+    {
+        public class Node
+        {
+            public int val;
+            public Node left;
+            public Node right;
+            public Node next;
+
+            public Node() { }
+
+            public Node(int _val)
+            {
+                val = _val;
+            }
+
+            public Node(int _val, Node _left, Node _right, Node _next)
+            {
+                val = _val;
+                left = _left;
+                right = _right;
+                next = _next;
+            }
+        }
+
+        public Node Connect(Node root)
+        {
+            if (root == null) return null;
+
+            var queue = new Queue<Node>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                int size = queue.Count;
+                Node prev = null;
+
+                for (int i = 0; i < size; i++)
+                {
+                    var node = queue.Dequeue();
+                    if (prev != null) prev.next = node;
+                    prev = node;
+
+                    if (node.left != null) queue.Enqueue(node.left);
+                    if (node.right != null) queue.Enqueue(node.right);
+                }
+            }
+
+            return root;
+        }
+    }
+}
