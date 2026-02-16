13b03a56a4fcd20b5260730177b81dd123b09c9d
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-02-16 21:06:23 +0200

    new problems

---
+++ LeetCode.Solutions/Top150/Topic08_BinarySearchTree/Path Sum.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Solutions.Top150.Topic08_BinarySearchTree
+{
+    internal class Path_Sum
+    {
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+            if (root == null)
+                return false;
+            if (root.left == null && root.right == null)
+                return targetSum == root.val;
+
+            bool leftSum = HasPathSum(root.left, targetSum - root.val);
+            bool rightSum = HasPathSum(root.right, targetSum - root.val);
+
+            return leftSum || rightSum;
+
+
+
+        }
+    }
+}
---
+++ LeetCode.Solutions/Top150/Topic08_BinarySearchTree/Sum Root to Leaf Numbers.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Solutions.Top150.Topic08_BinarySearchTree
+{
+    internal class Sum_Root_to_Leaf_Numbers
+    {
+        public int SumNumbers(TreeNode root)
+        {
+            return Dfs(root, 0);
+        }
+
+        private int Dfs(TreeNode node, int num)
+        {
+            if (node == null) return 0;
+            if (node.left == null && node.right == null)
+            {
+                return num * 10 + node.val;
+            }
+            return Dfs(node.left, num * 10 + node.val) + Dfs(node.right, num * 10 + node.val);
+        }
+    }
+}
