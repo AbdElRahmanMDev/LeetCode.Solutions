c0b5545446fe687b58d717a55f1fd317ee2162b6
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-02-17 17:16:58 +0200

    Longest Common Prefix

---
+++ LeetCode.Solutions/Top150/Topic01_Array/Longest Common Prefix.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Solutions.Top150.Topic01_Array
+{
+    internal class Longest_Common_Prefix
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            //    ["flower","flow","flight"]
+
+
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+                foreach (var str in strs)
+                {
+                    if (str.Length == i || str[i] != strs[0][i])
+                        return str.Substring(0, i);
+                }
+            }
+            return strs[0];
+        }
+    }
+}
