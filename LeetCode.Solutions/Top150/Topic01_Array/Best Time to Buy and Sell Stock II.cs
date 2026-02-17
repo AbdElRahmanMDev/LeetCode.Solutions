7ab786dd09cc818d1a0c7738a4b207f0d52af699
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-02-17 16:23:49 +0200

    Problem of Today

---
+++ LeetCode.Solutions/Top150/Topic01_Array/Best Time to Buy and Sell Stock II.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Solutions.Top150.Topic01_Array
+{
+    internal class Best_Time_to_Buy_and_Sell_Stock_II
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int profit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > prices[i - 1])
+                {
+                    profit += prices[i] - prices[i - 1];
+                }
+            }
+            return profit;
+        }
+    }
+}
