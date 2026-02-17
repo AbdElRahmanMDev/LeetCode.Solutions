744cbf2df0ba49e44a95ba88dcd373d81f77b98a
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-02-17 13:51:31 +0200

    Best Time to Buy and Sell Stock

---
+++ LeetCode.Solutions/Top150/Topic01_Array/Best Time to Buy and Sell Stock.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Solutions.Top150.Topic01_Array
+{
+    internal class Best_Time_to_Buy_and_Sell_Stock
+    {
+        public int MaxProfit(int[] prices)
+        {
+
+            int maxProfit = 0;
+            int buy = 0;
+            int sell = 1;
+            while (sell < prices.Length)
+            {
+                if (prices[sell] > prices[buy])
+                {
+                    maxProfit = Math.Max(maxProfit, prices[sell] - prices[buy]);
+                }
+                else
+                {
+                    buy = sell;
+                }
+                sell++;
+            }
+
+            return maxProfit;
+        }
+    }
+}
