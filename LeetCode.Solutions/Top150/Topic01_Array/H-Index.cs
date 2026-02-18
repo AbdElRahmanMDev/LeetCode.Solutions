1f767cd9425386d06476ec0636bd0860a876423f
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-02-18 18:46:55 +0200

    h-index and find the Highest Altitude

---
+++ LeetCode.Solutions/Top150/Topic01_Array/H-Index.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Solutions.Top150.Topic01_Array
+{
+    internal class H_Index
+    {
+        public int HIndex(int[] citations)
+        {
+            //[0,1,3,5,6]
+            int n = citations.Length;
+            Array.Sort(citations);
+            for (int i = 0; i < n; i++)
+            {
+                if (citations[i] >= n - i) return n - i;
+            }
+            return 0;
+        }
+    }
+}
---
+++ LeetCode.Solutions/Top75/Topic00_Prefix Sum/Find the Highest Altitude.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Solutions.Top75.Topic00_Prefix_Sum
+{
+    internal class Find_the_Highest_Altitude
+    {
+        public int LargestAltitude(int[] gain)
+        {
+            int max = 0;
+            int sum = 0;
+            for (int i = 0; i < gain.Length; i++)
+            {
+                sum += gain[i];
+                max = Math.Max(max, sum);
+            }
+
+            return max;
+        }
+    }
+}
