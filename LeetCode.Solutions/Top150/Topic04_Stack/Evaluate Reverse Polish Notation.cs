ffd7e55a7a6888976cc63acbce8d4f8c68d33658
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-02-20 02:55:03 +0200

    New Problems in stack

---
+++ LeetCode.Solutions/Top150/Topic04_Stack/Evaluate Reverse Polish Notation.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Solutions.Top150.Topic04_Stack
+{
+    internal class Evaluate_Reverse_Polish_Notation
+    {
+        public int EvalRPN(string[] tokens)
+        {
+            Stack<int> stack = new Stack<int>();
+            foreach (string c in tokens)
+            {
+                if (c == "+")
+                {
+                    stack.Push(stack.Pop() + stack.Pop());
+                }
+                else if (c == "-")
+                {
+                    int a = stack.Pop();
+                    int b = stack.Pop();
+                    stack.Push(b - a);
+                }
+                else if (c == "*")
+                {
+                    stack.Push(stack.Pop() * stack.Pop());
+                }
+                else if (c == "/")
+                {
+                    int a = stack.Pop();
+                    int b = stack.Pop();
+                    stack.Push((int)((double)b / a));
+                }
+                else
+                {
+                    stack.Push(int.Parse(c));
+                }
+            }
+            return stack.Pop();
+
+        }
+    }
+}
