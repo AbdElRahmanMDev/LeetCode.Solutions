1fa59b09a0266c3b895c23e18bfe415369730f41
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-02-22 05:26:04 +0200

    new BST Problem

---
+++ LeetCode.Solutions/Top150/Topic08_BinarySearchTree/Validate Binary Search Tree.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Solutions.Top150.Topic08_BinarySearchTree
+{
+    internal class Validate_Binary_Search_Tree
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            return valid(root, long.MinValue, long.MaxValue);
+        }
+
+        public bool valid(TreeNode root, long min, long max)
+        {
+            if (root == null)
+                return true;
+            if (!(root.val > min && root.val < max))
+            {
+                return false;
+            }
+            bool left = valid(root.left, min, root.val);
+            bool right = valid(root.right, root.val, max);
+            return left && right;
+        }
+    }
+}
