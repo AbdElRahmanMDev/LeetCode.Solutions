befcb517d77811dccd9597f380679aef6057ad79
AbdElRahmanMDev <abdelrahman.m.elsayedd@gmail.com>
2026-02-22 05:01:59 +0200

    kth Smallest Elment

---
+++ LeetCode.Solutions/Top150/Topic08_BinarySearchTree/Kth Smallest Element in a BST.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Solutions.Top150.Topic08_BinarySearchTree
+{
+    internal class Kth_Smallest_Element_in_a_BST
+    {
+        public int KthSmallest(TreeNode root, int k)
+        {
+            TreeNode current = root;
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            while (current != null || stack.Count > 0)
+            {
+                while (current != null)
+                {
+                    stack.Push(current);
+                    current = current.left;
+                }
+                current = stack.Pop();
+                k--;
+                if (k == 0)
+                    return current.val;
+
+                current = current.right;
+            }
+            return -1;
+
+        }
+    }
+}
